ca28f8efa978e1374da449524173ceb89614b402
ferminmu26 <89594515+ferminmu26@users.noreply.github.com>
2022-02-02 19:27:11 -0300

    Created Mobile popup

--- script.js
+++ script.js
@@ -15,3 +15,43 @@ function MobileMenu() {
 }
 toggle.addEventListener('click', MobileMenu);
 link.addEventListener('click', MobileMenu);
+
+
+
+const portfolio = [
+  {
+  'id': 'c1', 
+  'title':'Tonic',
+  'client':'CANOPY',
+  'role':'Back End Dev',
+  'year':'2015',
+  'description':"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"
+  },
+  {
+    'id': 'c2', 
+    'title':'Multi-Post Stories',
+    'client':'FACEBOOK',
+    'role':'Full Stack Dev',
+    'year':'2015',
+    'description':"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"
+    },
+    {
+      'id': 'c3', 
+      'title':'Facebook 360',
+      'client':'FACEBOOK',
+      'role':'Full Stack Dev',
+      'year':'2015',
+      'description':"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"
+      },
+      {
+        'id': 'c4', 
+        'title':'Uber Navigation',
+        'client':'Uber',
+        'role':'Lead Developer',
+        'year':'2018',
+        'description':"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"
+        },
+      
+
+
+]
